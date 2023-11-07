5e787d84bc55d1122dabdcf4a5c5399b6c1c42bc
Teko13 <ffabrice999@gmail.com>
2023-11-07 12:45:27 +0100

    Improvement of the fixtures data and the homeController

--- src/Controller/HomePage.php
+++ src/Controller/HomePage.php
@@ -13,15 +13,7 @@ class HomePage extends AbstractController {
     #[Route("/", name:"home_page")]
     public function homePage() {
         $trickRepository = $this->em->getRepository(Trick::class);
-        $tricksArray = $trickRepository->findAll();
-        $tricks = array();
-        foreach($tricksArray as $trick) {
-            $trickImgs = $trick->getFiles();
-            $tricks[] = [
-                'trick' => $trick,
-                "trick_img" => $trickImgs[mt_rand(0, 2)]
-            ];
-        }
-        return $this->render("home_page.html.twig", ["tricks" => $tricks]);
+        $tricks = $trickRepository->findAll();
+        return $this->render("home_page.html.twig", compact("tricks"));
     }
 }
